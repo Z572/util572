e03dcb82458c6972b0e2432604be419234bbe246
Z572 <873216071@qq.com>
2022-10-09 22:14:24 +0800

    string.scm: return origin string if LENGTH is 0.

--- util572/string.scm
+++ util572/string.scm
@@ -3,7 +3,10 @@
   #:export (string-split-length))
 
 (define (string-split-length s length)
-  (let loop ((s s))
-    (if (> (string-length s ) length)
-        (cons (string-take s length) (loop (string-drop s length)))
-        (list s))))
+  (if (zero? length)
+      s
+      (let loop ((s s))
+        (if (> (string-length s ) length)
+            (cons (string-take s length)
+                  (loop (string-drop s length)))
+            (list s)))))
